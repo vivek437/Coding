ccd9b2495b0a762f4f50c7ac0eb5fdd3d2d039d7
Vivek Prasad <vivekprasad345@gmail.com>
2021-03-31 00:39:26 +0530

    Create TopDown.java

---
+++ Dynamic Programming/LowestCommonSubsequence/TopDown.java
@@ -0,0 +1,27 @@
+
+
+/*
+https://practice.geeksforgeeks.org/problems/longest-common-subsequence-1587115620/1
+*/
+
+class Solution{
+    // function to find LCS
+    
+    
+    static int lcs(int p, int q, String s1, String s2){
+        
+         int dp[][] = new int[p+1][q+1];
+      
+          for(int i=1;i<=p;i++){
+             for(int j=1;j<=q;j++){
+                
+                if(s1.charAt(i-1) == s2.charAt(j-1))
+                     dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                     dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+             }
+         }
+         
+         return dp[p][q];
+    }
+}
