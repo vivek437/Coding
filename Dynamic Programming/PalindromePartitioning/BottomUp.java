1a165d5a1dca18e0e38806dc13cca0ce50cc0b86
Vivek Prasad <vivekprasad345@gmail.com>
2021-04-07 02:01:07 +0530

    Create BottomUp.java

---
+++ Dynamic Programming/PalindromePartitioning/BottomUp.java
@@ -0,0 +1,59 @@
+
+/* 
+https://practice.geeksforgeeks.org/problems/palindromic-patitioning4845/1#
+*/
+class Solution{
+    
+    static boolean isPalindrome(String string, int i, int j)
+    {
+        while(i < j)
+        {
+          if(string.charAt(i) != string.charAt(j))
+             return false; 
+          i++;
+          j--;
+        }
+        return true;
+    }
+    
+    static int dp[][];
+    static int palindromicPartition(String str)
+    {
+          int  l = str.length();
+          dp = new int[l+1][l+1];
+          
+          for(int i=0;i<l;i++){
+              for(int j=0;j<l;j++){
+                  dp[i][j] = -1;
+              }
+          }
+          
+          return solve(str,0,l-1);
+    }
+    
+    static int solve(String str,int i,int j){
+          
+          if(i>j)
+            return 0;
+          
+          if(dp[i][j] !=-1)
+             return dp[i][j];
+          
+          if(isPalindrome(str,i,j) == true){
+               dp[i][j] = 0;
+               return 0;
+          }
+          
+          int min = Integer.MAX_VALUE; 
+          for(int k=i;k<j;k++){
+              
+             int temp = 1 +  solve(str,i,k) + solve(str,k+1,j);
+             if(temp<min)
+                min = temp;
+          }
+          
+          dp[i][j] = min;
+          return dp[i][j];
+    }
+    
+}
