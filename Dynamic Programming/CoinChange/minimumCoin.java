40a5715f536ae4ac0bd30a0b6b40238e88b57059
vprasad <Vivek.Prasad@hyland.com>
2021-04-11 01:11:41 +0530

    MinimumCoins

---
+++ Dynamic Programming/CoinChange/minimumCoin.java
@@ -0,0 +1,21 @@
+
+     public int minimumCoinBottomUp(int total, int coins[]){
+        int T[] = new int[total + 1];
+        int R[] = new int[total + 1];
+        T[0] = 0;
+        for(int i=1; i <= total; i++){
+            T[i] = Integer.MAX_VALUE-1;
+            R[i] = -1;
+        }
+        for(int j=0; j < coins.length; j++){
+            for(int i=1; i <= total; i++){
+                if(i >= coins[j]){
+                    if (T[i - coins[j]] + 1 < T[i]) {
+                        T[i] = 1 + T[i - coins[j]];
+                        R[i] = j;
+                    }
+                }
+            }
+        }
+        return T[total];
+    }
