167b23be9eea99985758f00f1643fea25bf5fb26
Vivek Prasad <vivekprasad345@gmail.com>
2021-05-22 12:59:13 +0530

    Create RemoveBSTOutsideOfGivenRange.java

---
+++ Binary Search Tree/RemoveBSTOutsideOfGivenRange.java
@@ -0,0 +1,44 @@
+
+/*
+https://practice.geeksforgeeks.org/problems/remove-bst-keys-outside-given-range/1
+*/
+class Solution {
+    
+    List<Integer> ar = new ArrayList<>();
+    Node removekeys(Node root, int l, int r) {
+        // code here
+        inOrder(root,l,r);
+        return formBST(ar,0,ar.size()-1);
+       
+    }
+    
+    Node formBST(List<Integer> ar, int st, int en) {
+        
+        if(st>en)
+           return null;
+        
+        int mid = (st + en)/2;
+       
+        Node temp = new Node(ar.get(mid));
+       
+        temp.left = formBST(ar,st,mid-1);
+        temp.right = formBST(ar,mid+1,en);
+        
+        return temp;
+    }
+    
+    
+    void inOrder(Node node,int l,int r)
+    {
+          
+          if(node == null)
+             return;
+             
+           inOrder(node.left,l,r);
+           
+           if(node.data>=l && node.data<=r)
+              ar.add(node.data);
+              
+           inOrder(node.right,l,r);
+    }
+}
